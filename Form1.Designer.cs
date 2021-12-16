14946aef60e65d9e3339a2e6716060b788774eca
Jakepps <nagalevsiy@mail.ru>
2021-12-16 21:25:47 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace laba16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(721, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clean";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(751, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(751, 134);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(721, 209);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(704, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(704, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "B:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 582);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace laba16
+{
+    public partial class Form1 : Form
+    {
+        ZedGraphControl zedGrapgControl1 = new ZedGraphControl();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            zedGrapgControl1.Location = new Point(10, 30);
+            zedGrapgControl1.Name = "text";
+            zedGrapgControl1.Size = new Size(500, 500);
+            Controls.Add(zedGrapgControl1);
+            GraphPane my_Pane = zedGrapgControl1.GraphPane;
+            my_Pane.Title.Text = "Результат";
+            my_Pane.XAxis.Title.Text = "X";
+            my_Pane.YAxis.Title.Text = "Y";
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear(zedGrapgControl1);
+        }
+        private void Clear(ZedGraphControl Zed_GraphControl)
+        {
+            //GraphPane pane = Zed_GraphControl.GraphPane;
+            zedGrapgControl1.GraphPane.CurveList.Clear();
+            zedGrapgControl1.GraphPane.GraphObjList.Clear();
+
+            zedGrapgControl1.GraphPane.XAxis.Type = AxisType.Linear;
+            zedGrapgControl1.GraphPane.XAxis.Scale.TextLabels = null;
+            zedGrapgControl1.GraphPane.XAxis.MajorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.YAxis.MajorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.YAxis.MinorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.XAxis.MinorGrid.IsVisible = false;
+            zedGrapgControl1.RestoreScale(zedGrapgControl1.GraphPane);
+
+            zedGrapgControl1.AxisChange();
+            zedGrapgControl1.Invalidate();
+        }
+        static double f1(double x, double a, double b)
+        {
+            return Math.Sin(x+a*b);
+        }
+        private void Ris(ZedGraphControl Zed_GraphControl)
+        {
+            GraphPane my_Pane = Zed_GraphControl.GraphPane;
+
+            PointPairList list = new PointPairList();
+            List<double> X = new List<double>();
+            List<double> Y = new List<double>();
+            double h = 0.1, x = 0.8, a, b;
+
+            try
+            {
+                a = Convert.ToDouble(textBox1.Text);
+                b = Convert.ToDouble(textBox2.Text);
+
+                double minY = 0, maxY = 0;
+
+
+                while (x <= 100.0)
+                {
+
+                    list.Add(x, f1(x, a, b));
+                    X.Add(x); 
+                    Y.Add(f1(x, a, b));
+                    x += h;
+                }
+                int minXpos = 0;
+                int maxXpos = 0;
+                for (int i = 0; i < Y.Count; i++)
+                {
+                    if (minY < Y[i])
+                    {
+                        minXpos = i;
+                        minY = Y[i];
+                    }
+                    if (maxY > Y[i])
+                    {
+                        maxXpos = i;
+                        maxY = Y[i];
+                    }
+                }
+                double minX = X[minXpos];
+                double maxX = X[maxXpos];
+
+                PointPairList listMIN = new PointPairList();
+                PointPairList listMAX = new PointPairList();
+                for (double i = -5; i < 5; i += 0.1)
+                {
+                    listMIN.Add(minX, i);
+                }
+                for (double i = -5; i < 5; i += 0.1)
+                {
+                    listMAX.Add(maxX, i);
+                }
+                LineItem d1 = my_Pane.AddCurve("Функция", list, Color.Green, SymbolType.Circle);
+                LineItem d2 = my_Pane.AddCurve("Правая граница", listMIN, Color.Orange, SymbolType.Diamond);
+                LineItem d3 = my_Pane.AddCurve("Левая граница", listMAX, Color.Purple, SymbolType.Diamond);
+
+                zedGrapgControl1.AxisChange();
+                zedGrapgControl1.Invalidate();
+            }
+            catch
+            {
+
+                MessageBox.Show("Некорректный ввод данных");
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            sender = textBox1;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            sender = textBox2;
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clear(zedGrapgControl1);
+            Ris(zedGrapgControl1);
+        }
+    }
+}
